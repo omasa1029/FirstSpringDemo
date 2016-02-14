46b2603fe50f1457f55acd5226724b21f1129883
Masashi <o.masa.1029+github@gmail.com>
2016-02-14 20:59:16 +0900

    各種更新。

---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String name;
+
+}
--- src/test/java/com/example/FirstSpringDemoApplicationTests.java
+++ src/test/java/com/example/FirstSpringDemoApplicationTests.java
@@ -1,10 +1,19 @@
 package com.example;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.example.controller.HelloCounter;
+import com.example.entity.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = FirstSpringDemoApplication.class)
@@ -15,4 +24,24 @@ public class FirstSpringDemoApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void test_getUserList_a() {
+		User u1 = new User() {
+			{
+				setId("1");
+				setName("Tanaka");
+			}
+		};
+		User u2 = new User() {
+			{
+				setId("2");
+				setName("Satou");
+			}
+		};
+
+		List<User> expected = Arrays.asList(u1, u2);
+		List<User> actual = new HelloCounter().getUserList();
+
+		assertThat(actual, is(expected));
+	}
 }
